f775ab9d2b51d17e2a49e46f68273466bba612b4
VipulAggarwal2003 <vipulthebest123@gmail.com>
2024-09-23 12:24:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: [
-    "https://job-portal-using-mern.onrender.com/",
-    `${process.env.BASE_URL}`,
-  ],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: [
+//     "https://job-portal-using-mern.onrender.com/",
+//     `${process.env.BASE_URL}`,
+//   ],
+//   credentials: true,
+// };
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // API's
