cccf7e4388528dd883263829e4289ea7f5bb844e
Vipul Aggarwal <vipulthebest123@gmail.com>
2025-02-17 12:21:16 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  // origin: [`${process.env.BASE_URL}`],
+  //origin: [`${process.env.BASE_URL}`],
   origin:"*"
 };
 app.use(cors(corsOptions));
